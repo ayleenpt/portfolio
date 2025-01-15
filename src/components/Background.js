5cf469a1b80624a4ef27e213a6c21aa1a8afebbb
Ayleen <ayleenpiteotarpy@gmail.com>
2025-01-15 11:49:33 -0800

    add dynamic background

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,11 +1,33 @@
 import React from 'react';
-import '../styles/Background.css';
+import '../styles/Background.scss';
+import Glass from './Glass';
 
-function Background() {
+const Background = () => {
   return (
-    <div className="page">
+    <div className="background">
+      <svg>
+        <filter id="goo">
+          <feGaussianBlur in="SourceGraphic" stdDeviation="30" result="blur" />
+          <feColorMatrix
+            in="blur"
+            type="matrix"
+            values="
+              1 0 0 0 0
+              0 1 0 0 0
+              0 0 1 0 0
+              0 0 0 30 -10"
+          />
+        </filter>
+      </svg>
+      <div className="blob-container">
+        <div className="blob blob1"></div>
+        <div className="blob blob2"></div>
+        <div className="blob blob3"></div>
+        <div className="blob blob4"></div>
+        <div className="blob blob5"></div>
+      </div>
     </div>
   );
-}
+};
 
 export default Background;
---
+++ src/components/Glass.js
@@ -0,0 +1,12 @@
+function Glass() {
+  return (
+    <div className="glass">
+      <div className="glass__content">
+        <h1>Glass</h1>
+        <p>Material Design Glassmorphism in React</p>
+      </div>
+    </div>
+  );
+}
+
+export default Glass;

521b679259b8825e34e9813329b5fbaca6e8d520
Ayleen <ayleenpiteotarpy@gmail.com>
2025-01-15 16:40:00 -0800

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
 import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }

d259247390693859b5d9a42d71c71d16fe98449f
Ayleen <ayleenpiteotarpy@gmail.com>
2025-01-23 19:44:49 -0800

    icon maker project page

--- src/components/projects/IconMaker.js
+++ src/components/projects/IconMaker.js
@@ -1,17 +1,23 @@
 import { useLocation } from "react-router-dom";
 import React from 'react';
-import Background from "../page/Background";
-import Menu from '../page/Menu';
+import Homescreen from '../../assets/projects/iconmaker/Homescreen.png';
+import ProjectPage from './ProjectPage';
 
 function IconMaker() {
   const location = useLocation();
-
+  const description = "Icon Maker was a small, single day project I made when I was bored, and wanted to re-familiarize myself with React.js by making a silly app to show my friends. It involves a few different selection of transparent images that stack and compress into a single downloadable image. I also created all of the art present in this project, so it was a fun intersection of digital and traditional art.";
   return (
-    <div className="icon-maker">
-      <Background />
-      <h1>Icon Maker</h1>
-      <Menu key={location.pathname} />
-    </div>
+    <ProjectPage
+      title="Icon Maker"
+      date="Jan 2025"
+      description={description}
+      homescreen={Homescreen}
+      projectTools="React, Javascript, HTML, CSS, GitHub Pages"
+      location={location}
+      visitProject="Make your own icon"
+      projectLink="https://ayleenpt.github.io/icon_maker/"
+    >
+    </ProjectPage>
   );
 }
 

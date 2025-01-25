7a6391246f9e84d347abab0dcc25353e5c2e3520
Ayleen <ayleenpiteotarpy@gmail.com>
2025-01-24 19:53:07 -0800

    update aurora description and date

--- src/components/projects/Aurora.js
+++ src/components/projects/Aurora.js
@@ -5,11 +5,11 @@ import ProjectPage from './ProjectPage';
 
 function Aurora() {
   const location = useLocation();
-  const description = "Aurora features a night sky with twinkling stars that react to the movements of the mouse. Bringing the stars behavior to life was challenging, but through many cycles of research, trial, and iteration, I achieved the interactive effect I had imagined, even adding interesting effects for when the mouse is still. I optimized the site’s performance by minimizing re-renders and streamlining the animations that create the twinkling effect, leading to a simpler and more efficient final product. Aurora was a rewarding challenge that remains a flexible project, with room for me to continue building off of and improving as I develop my talents.";
+  const description = "Aurora features a night sky with twinkling stars that react to the movements of the mouse. Bringing the stars behavior to life was challenging, but through many cycles of research, trial, and iteration, I achieved the interactive effect I had imagined, even adding interesting effects for when the mouse is still. I optimized the site’s performance by minimizing re-renders and streamlining the animations that create the twinkling effect, leading to a simpler and more efficient final product. Aurora currently works best on Firefox, but this is an active project with incoming updates to optimize performance universally.";
   return (
     <ProjectPage
       title="Aurora"
-      date="Jan 2025"
+      date="Jan 2025 - Present"
       description={description}
       homescreen={Homescreen}
       projectTools="React, Javascript, HTML, CSS, GitHub Pages"

d7eee47d1aefb7105d16953a563c973733cd1d71
Ayleen <ayleenpiteotarpy@gmail.com>
2025-01-24 17:01:28 -0800

    add right lick -> open link in new tab functionality

--- src/components/projects/ProjectLink.js
+++ src/components/projects/ProjectLink.js
@@ -3,13 +3,14 @@ import '../../styles/projects/ProjectLink.css';
 
 function ProjectLink({ projectName, thumbnail, link }) {
   const navigate = useNavigate();
-  const handleClick = () => { navigate(`/projects${link}`); };
 
   return (
-    <div className="project-link" onClick={handleClick}>
-      <img className="thumbnail" src={thumbnail} alt={projectName} />
-      <h1 className="project-name">{projectName}</h1>
-    </div>
+    <a href={`https://ayleenpt.github.io/portfolio/#/projects${link}`}>
+      <div className="project-link">
+        <img className="thumbnail" src={thumbnail} alt={projectName} />
+        <h1 className="project-name">{projectName}</h1>
+      </div>
+    </a>
   );
 }
 

42ed9aacd62216316e7250d43394eaf2d40f5979
Ayleen <ayleenpiteotarpy@gmail.com>
2025-02-04 16:49:54 -0800

    adjust Name.js to take custom styles, add bio and headshot to about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import Background from "../page/Background";
 import Menu from '../page/Menu';
+import Name from '../home/Name';
+import Footer from '../page/Footer';
+import Headshot from '../../assets/about/Headshot.jpg';
 import '../../styles/about/About.css';
 
 function About() {
   return (
     <div className="about">
       <Background />
-      <a className="link" href="https://en.wikipedia.org/wiki/The_Witch_(2015_film)">learn about ayleen here</a>
       <Menu />
+      <div className="bio-container">
+        <img className="headshot" src={ Headshot } alt="headshot"></img>
+        <div className="bio">
+          <Name
+            nameStyle={{
+              alignItems: 'var(--alignment)'
+            }}
+            letterStyle={{
+              height: 'var(--letter-height)'
+            }}
+          />
+          <div className="bio-text">
+          I'm a senior at UW Bothell, graduating in June 2025 with a B.S. in Computer Science and Software Engineering. Beyond academics, I’ve tutored pre-major students through UW Bothell’s State Academic Rising Scholars (STARS) program and interned at Chewy as a backend software developer. In my free time I enjoy doing crafts with my friends, crocheting outfits for my cats, playing video games, and trying new food around Seattle.
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
--- src/components/home/Name.js
+++ src/components/home/Name.js
@@ -6,27 +6,27 @@ import E from '../../assets/name/E.png';
 import N from '../../assets/name/N.png';
 import '../../styles/home/Name.css';
 
-function Name() {
+function Name({ nameStyle, letterStyle }) {
   return (
-    <div className="name">
+    <div className="name" style={ nameStyle }>
       <div className="text intro">hello, my name is</div>
       <div className="letters">
-        <div className="letter one">
+        <div className="letter one" style={ letterStyle } >
           <img src={A} alt="A" />
         </div>
-        <div className="letter two">
+        <div className="letter two" style={ letterStyle } >
           <img src={Y} alt="Y" />
         </div>
-        <div className="letter three">
+        <div className="letter three" style={ letterStyle } >
           <img src={L} alt="L" />
         </div>
-        <div className="letter one">
+        <div className="letter one" style={ letterStyle } >
           <img src={E} alt="E" />
         </div>
-        <div className="letter three">
+        <div className="letter three" style={ letterStyle } >
           <img src={E} alt="E" />
         </div>
-        <div className="letter two">
+        <div className="letter two" style={ letterStyle } >
           <img src={N} alt="N" />
         </div>
       </div>

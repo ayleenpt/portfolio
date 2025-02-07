5cb4100ff69d32542fa2da6e15768301db09ce44
Ayleen <ayleenpiteotarpy@gmail.com>
2025-02-06 19:42:02 -0800

    style contact form

--- src/components/about/Contact.js
+++ src/components/about/Contact.js
@@ -2,9 +2,27 @@ import '../../styles/about/Contact.css';
 
 function Contact () {
   return(
-    <div className="contact">
-      contact!
-    </div>
+    <section className="contact">
+      <form>
+        <h2>Get In Touch</h2>
+        <div className="input-box">
+          <div className="label-text">Full Name</div>
+          <input type="text" className="field" placeholder="your name" required />
+        </div>
+
+        <div className="input-box">
+          <div className="label-text">Email Address</div>
+          <input type="text" className="field" placeholder="example@email.com" required />
+        </div>
+
+        <div className="input-box">
+          <div className="label-text">Message</div>
+          <textarea type="text" className="field message" placeholder="your message" required></textarea>
+        </div>
+
+        <button type="submit">Send Message</button>
+      </form>
+    </section>
   );
 }
 

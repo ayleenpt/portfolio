61b05f643475f6d5416393cee64662fd54f3349b
Ayleen <ayleenpiteotarpy@gmail.com>
2025-02-15 22:06:48 -0800

    finish play button display

---
+++ src/components/projects/notestoself/NotesToSelf.js
@@ -0,0 +1,42 @@
+import { useLocation } from "react-router-dom";
+import React from 'react';
+import PlayButton from './PlayButton';
+import ProjectPage from '../ProjectPage';
+import ProgressBar from '../../../assets/projects/notestoself/ProgressBar.png';
+import Homescreen from '../../../assets/projects/notestoself/Homescreen.png';
+import '../../../styles/projects/notestoself/NotesToSelf.css';
+
+function bonusContent() {
+  return(
+    <div className="project-features" >
+      <div className="features-title">Features</div>
+        <PlayButton />
+        <div className="progress-bar">
+        <img src={ProgressBar} alt="progress bar"/>
+        <text>Dynamic progress bar</text>
+      </div>
+    </div>
+  );
+}
+
+function NotesToSelf() {
+  const location = useLocation();
+  const description = "Notes to Self was a limited release art piece commissioned by and created in collaboration with musician Amelia Day. It contained demos of five unreleased EPs and a store front where users could purchase votes with merch prizes, and then place their votes on an EP for Amelia to fully produce. This was my first ever web development project, and my first public facing software creation. It was built over the course of two months with a continuous feedback loop between myself and Amelia, with iterative development and improvement as the design came together.";
+  
+  return (
+    <div>
+      <ProjectPage
+        title="Notes to Self"
+        date="Jun-Jul 2024"
+        description={description}
+        homescreen={Homescreen}
+        projectTools="React, Javascript, HTML, CSS, GitHub Pages"
+        location={location}
+        bonusContent={bonusContent()}
+      >
+      </ProjectPage>
+    </div>
+  );
+}
+
+export default NotesToSelf;
---
+++ src/components/projects/notestoself/PlayButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PlayButtonStatic from '../../../assets/projects/notestoself/PlayButton.png';
+import PlayButtonBG from '../../../assets/projects/notestoself/PlayButtonBG.png';
+import PlayButtonActive from '../../../assets/projects/notestoself/PlayButtonActiveBG.png'
+import '../../../styles/projects/notestoself/PlayButton.css';
+
+function PlayButton() {
+  const handleHover = () => {
+    const playButtonActive = document.querySelector('.play-button-active');
+    playButtonActive.style.backgroundImage = `url(${PlayButtonActive})`;
+  };
+
+  const handleLeave = () => {
+    const playButtonActive = document.querySelector('.play-button-active');
+    playButtonActive.style.backgroundImage = "";
+  };
+
+  return (
+    <div className="play-button-wrapper">
+      <text>Custom play buttons with responsive active state.</text>
+      <div className="play-button image-wrapper">
+        <img src={PlayButtonBG} alt="play button" />
+        <div className="play-button-active" />
+        <img className="play-button-img" src={PlayButtonStatic} alt="play button" />
+        <div className="play-button-active-area" onMouseEnter={handleHover} onMouseLeave={handleLeave} />
+      </div>
+    </div>
+  );
+}
+
+export default PlayButton;

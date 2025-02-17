fdc610ad426e0548f38b1a7ce7d120db0c586da3
Ayleen <ayleenpiteotarpy@gmail.com>
2025-02-16 22:06:35 -0800

    add backgrounds to progress bar and storefront displays

--- src/components/projects/notestoself/ProgressBar.js
+++ src/components/projects/notestoself/ProgressBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Background from '../../../assets/projects/notestoself/LongBG.png';
 import '../../../styles/projects/notestoself/ProgressBar.css';
 
 function ProgressBar() {
@@ -8,13 +9,16 @@ function ProgressBar() {
     setValue(event.target.value);
   };
   return (
-    <div className="progress-bar">
-      <input
-        type="range"
-        value={value}
-        onChange={handleChange}
-        className="custom-slider"
-      />
+    <div className="progress-bar-wrapper">
+      <div className="progress-bar image-wrapper">
+        <img src={Background} alt="progress bar" />
+        <input
+          type="range"
+          value={value}
+          onChange={handleChange}
+          className="custom-slider"
+        />
+      </div>
       <text>Dynamic progress bar</text>
     </div>
   );
--- src/components/projects/notestoself/Storefront.js
+++ src/components/projects/notestoself/Storefront.js
@@ -1,31 +1,57 @@
+import { useState, useEffect } from 'react';
 import Thumbnail from '../../../assets/projects/notestoself/Thumbnail.png';
-import Wood from '../../../assets/projects/notestoself/Wood.png';
+import WideBG from '../../../assets/projects/notestoself/WideBG.png';
+import TallBG from '../../../assets/projects/notestoself/TallBG.png';
 import '../../../styles/projects/notestoself/Storefront.css';
 
 function Storefront() {
+  const [background, setBackground] = useState(TallBG);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 800) {
+        setBackground(WideBG);
+      } else {
+        setBackground(TallBG);
+      }
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className="storefront-wrapper">
 
-      <div className="storefront">
-        <div className="thumbnail">
-          <img src={ Thumbnail } alt="Notes to Self EP" />
-        </div>
+      <div className="storefront image-wrapper">
+        <img src={background} alt="storefront" />
 
-        <div className="item-title">
-          DIGITAL DOWNLOAD - Notes to Self EP
-          <br /><br />
-          Worth 2 votes
-        </div>
+        <div className="storefront-content">
+          <div className="thumbnail">
+            <img src={ Thumbnail } alt="Notes to Self EP" />
+          </div>
 
-        <div className="item-description">
-          Acoustic demos of each track on the "Notes to Self" project, as well as an exclusive voice memo track.
-        </div>
+          <div className="item-title">
+            DIGITAL DOWNLOAD - Notes to Self EP
+            <br /><br />
+            Worth 2 votes
+          </div>
 
-        <div className="price">
-          $10
-          <div className="purchase-button">Add to cart</div>
+          <div className="item-description">
+            Acoustic demos of each track on the "Notes to Self" project, as well as an exclusive voice memo track.
+          </div>
+
+          <div className="price">
+            $10
+            <div className="purchase-button">Add to cart</div>
+          </div>
         </div>
       </div>
+
       <div className="storefront-text">
         <text>Online storefront to purchase merchandise and votes</text>
       </div>

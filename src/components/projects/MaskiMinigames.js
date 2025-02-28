c0db0a347fbe89252c9c509728ee5c06c3b8cd38
Ayleen <ayleenpiteotarpy@gmail.com>
2025-02-28 14:18:32 -0800

    add collaborators to maski minigames

--- src/components/projects/MaskiMinigames.js
+++ src/components/projects/MaskiMinigames.js
@@ -6,13 +6,19 @@ import ProjectPage from './ProjectPage';
 function MaskiMinigames() {
   const location = useLocation();
   const description = "MASKI Minigames features two classic mini games developed in a collaborative project using React.js. I led the design and construction of the homepage, user interfaces, graphics, and page setup. I also assisted with decoupling and refactoring game components to streamline the final product, working iteratively with my team through refinement, development, and review sessions to deliver the final website you see now.";
+  const projectTools =
+    <div className="collaboration">
+      React, Javascript, HTML, CSS, GitHub Pages
+      <div className="created-with">In collaboration with</div>
+      Shaun Cushman, Khushmeet Gobindpuri, Ishaan Kapil, Minh Pham
+    </div>;
   return (
     <ProjectPage
       title="MASKI Minigames"
       date="Nov-Dec 2024"
       description={description}
       homescreen={Homescreen}
-      projectTools="React, Javascript, HTML, CSS, GitHub Pages"
+      projectTools={projectTools}
       location={location}
       visitProject="Visit MASKI Minigames"
       projectLink="https://bubseatbubs.github.io/maskiminigames/"
